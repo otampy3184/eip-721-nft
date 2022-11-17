72a3dadd230dc8b07917fc8fa0ecdbb4508d91ba
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-17 23:20:34 +0900

    update

--- test/EIP721NFT.ts
+++ test/EIP721NFT.ts
@@ -1,2 +1,13 @@
 const { expect } = require("chai");
+import { ethers } from "hardhat";
 
+describe("NFT", function() {
+    async function deployTest() {
+        const EIP721NFT = await ethers.getContractFactory("EIP721NFT");
+        const eip721nft = await EIP721NFT.deploy();
+
+        await eip721nft.deployed();
+
+        console.log("contract deployed at: ", eip721nft.address);
+    }
+})
